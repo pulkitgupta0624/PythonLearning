39738f06b7d3a7a96be928b1966af4ca038c9498
pulkitgupta0624 <pulkitgupta2403@gmail.com>
2026-01-22 20:59:27 +0530

    new added

---
+++ oops/6.staticMethods.py
@@ -0,0 +1,6 @@
+'''
+A static method is a method inside a class that:
+- does not use self (object)
+- does not use cls (class)
+- works like a normal function but kept inside class for better structure
+'''
